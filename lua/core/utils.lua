1c49436732f26d779d53f27d5da104b36fbf2919
danydodson <danydodson@gmail.com>
2026-02-15 20:16:09 -0600

    added tmux split terminal

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -16,6 +16,101 @@ function M.mappings(tbl, options)
   end
 end
 
+--
+--
+--
+--
+-- HACK: Neovim Toggle Terminal on Tmux Pane at the Bottom (or Right)
+-- https://youtu.be/33gQ9p-Zp0I
+--
+-- Toggle a tmux pane on the right in zsh, in the same directory as the current file
+--
+-- Notice I'm setting the variable DISABLE_PULL=1, because in my zshrc file,
+-- I check if this variable is set, if it is, I don't pull github repos, to save time
+--
+-- I keep track of the opened dir lamw25wmal, and if it changes, the next time I
+-- bring up the tmux pane, it will open the path of the new dir
+--
+-- I defined it as a function, because I call this function from the
+-- mini.files plugin to open the highlighted dir in a tmux pane on the right
+function M.tmux_pane_function(dir)
+  -- NOTE: variable that controls the auto-cd behavior
+  local auto_cd_to_new_dir = true
+  -- NOTE: Variable to control pane direction: 'right' or 'bottom'
+  -- If you modify this, make sure to also modify TMUX_PANE_DIRECTION in the
+  -- zsh-vi-mode section on the .zshrc file
+  -- Also modify this in your tmux.conf file if you want it to work when in tmux
+  -- copy-mode
+  local pane_direction = vim.g.tmux_pane_direction or "bottom"
+  -- NOTE: Below, the first number is the size of the pane if split horizontally,
+  -- the 2nd number is the size of the pane if split vertically
+  local pane_size = (pane_direction == "right") and 60 or 15
+  local move_key = (pane_direction == "bottom") and "C-l" or "C-k"
+  local split_cmd = (pane_direction == "right") and "-h" or "-v"
+  -- if no dir is passed, use the current file's directory
+  local file_dir = dir or vim.fn.expand("%:p:h")
+  -- Simplified this, was checking if a pane existed
+  local has_panes = vim.fn.system("tmux list-panes | wc -l"):gsub("%s+", "") ~= "1"
+  -- Check if the current pane is zoomed (maximized)
+  local is_zoomed = vim.fn.system("tmux display-message -p '#{window_zoomed_flag}'"):gsub("%s+", "") == "1"
+  -- Escape the directory path for shell
+  local escaped_dir = file_dir:gsub("'", "'\\''")
+  -- If any additional pane exists
+  if has_panes then
+    if is_zoomed then
+      -- Compare the stored pane directory with the current file directory
+      if auto_cd_to_new_dir and vim.g.tmux_pane_dir ~= escaped_dir then
+        -- If different, cd into the new dir
+        vim.fn.system("tmux send-keys -t :.+ 'cd \"" .. escaped_dir .. "\"' Enter")
+        -- Update the stored directory to the new one
+        vim.g.tmux_pane_dir = escaped_dir
+      end
+      -- If zoomed, unzoom and switch to the correct pane
+      vim.fn.system("tmux resize-pane -Z")
+      vim.fn.system("tmux send-keys " .. move_key)
+    else
+      -- If not zoomed, zoom current pane
+      vim.fn.system("tmux resize-pane -Z")
+    end
+  else
+    -- Store the initial directory in a Neovim variable
+    if vim.g.tmux_pane_dir == nil then
+      vim.g.tmux_pane_dir = escaped_dir
+    end
+    -- If no pane exists, open it with zsh and DISABLE_PULL variable
+    vim.fn.system(
+      "tmux split-window "
+      .. split_cmd
+      .. " -l "
+      .. pane_size
+      .. " 'cd \""
+      .. escaped_dir
+      .. "\" && DISABLE_PULL=1 zsh'"
+    )
+    vim.fn.system("tmux send-keys " .. move_key)
+    -- Resolve zsh-vi-mode issue for first-time pane
+    vim.fn.system("tmux send-keys Escape i")
+  end
+end
+
+-- If I execute the function without an argument, it will open the dir where the
+-- current file lives
+vim.keymap.set({ "n", "v", "i" }, "<M-t>", function() M.tmux_pane_function() end,
+  { desc = "[P]Terminal on tmux pane" })
+
+-- -- Open a tmux pane on the right in bash, in the same directory as the current file
+-- -- Opening it in bash because it's faster, I don't have to run my .zshrc file,
+-- -- which pulls from my repo and a lot of other stuff
+-- vim.keymap.set("n", "<leader>f.", function()
+--   local file_dir = vim.fn.expand("%:p:h") -- Get the directory of the current file
+--   -- `-l 60` specifies the size of the tmux pane, in this case 60 columns
+--   local cmd = "silent !tmux split-window -h -l 60 'cd " .. file_dir .. " && bash'"
+--   vim.cmd(cmd)
+-- end, { desc = "[P]Open current dir in right tmux pane" })
+--
+--
+--
+--
 -- run a shell command and capture the output and if the command succeeded or failed.
 function M.run_cmd(cmd, show_error)
   if type(cmd) == 'string' then
